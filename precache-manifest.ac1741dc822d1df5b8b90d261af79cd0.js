ce0db7ce18e45fbb3bc8a0c79eeefbd28442c830
rekha0suthar <rekha0suthar@gmail.com>
2024-01-03 17:07:03 +0530

    Updates

---
+++ precache-manifest.ac1741dc822d1df5b8b90d261af79cd0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "894b67730d11cdc9ebf4",
+    "url": "/E-commerce-web-application/static/css/main.cbe6969c.chunk.css"
+  },
+  {
+    "revision": "894b67730d11cdc9ebf4",
+    "url": "/E-commerce-web-application/static/js/main.894b6773.chunk.js"
+  },
+  {
+    "revision": "718ad8c65df9fb315c7f",
+    "url": "/E-commerce-web-application/static/js/runtime~main.718ad8c6.js"
+  },
+  {
+    "revision": "9701be6220839a3cca4e",
+    "url": "/E-commerce-web-application/static/js/2.9701be62.chunk.js"
+  },
+  {
+    "revision": "f021b9c3df9fdf2a24ab85a2eaf25aa2",
+    "url": "/E-commerce-web-application/index.html"
+  }
+];
